d92884161e932811eaafd17f6192534872d3b8d2
axd8911 <axd8911@hotmail.com>
2019-03-08 23:42:05 -0600

    Create 001_Two_Sum_1st_try.py

---
+++ 001_Two_Sum_1st_try.py
@@ -0,0 +1,22 @@
+'''
+Leetcode score: 76%
+Be aware that you refered to a solution.
+The main idea is using search of a dictionary. Should come back and repeat
+run time level (n)
+
+
+'''
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        lookup = {}
+        for index_1, num_1 in enumerate(nums):
+            #print (lookup)
+            if target - num_1 in lookup:
+                return [index_1, lookup[target-num_1]]
+                break
+            lookup.update({num_1:index_1})
+        
+        
