248c45fc5caf74c009b34a6efa43de07852b8ed8
axd8911 <axd8911@hotmail.com>
2019-03-15 13:31:56 -0500

    Update 036_Valid_Sudoku_1st_try.py

--- 036_Valid_Sudoku_1st_try.py
+++ 036_Valid_Sudoku_1st_try.py
@@ -1,17 +1,46 @@
 '''
 Two solutions below.
-Both are around 20-25%.
+Modified solution goes up to 82%
+The other is around 20-25%.
 
-Slightly away from main fastest range
-Should come back and check other's solutions
+Could make the code looks simplier
 .
 Comments:
 1.3*3 small cubes - how to represent by i and j
 2. List is slow. Other's may use without list
 3. for loop in for loop. any better run time level?
-'''
 
-#solution 1
+add up comments:
+1. learned how to use enumerate()
+2. 5//3 = 1, no need to use int to cast numbers
+
+'''
+#Modified solution
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        XY_set = {}  
+        for (i,row) in enumerate(board):
+            for (j,item) in enumerate(row):
+                if item != '.':
+                    if item not in XY_set:
+                        XY_set.update({item:[[i,j]]})
+                    else:
+                        temp_set = XY_set[item]
+                        if i in [ele[0] for ele in temp_set]:
+                            return False
+                        if j in [ele[1] for ele in temp_set]:
+                            return False
+                        for ele in temp_set:
+                            if ele[0]//3 == i//3 and ele[1]//3 == j//3:
+                                return False
+                        XY_set[item] += [[i,j]]
+                    
+        return True
+        
+
+
+#other solution
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
@@ -42,29 +71,6 @@ class Solution:
         return True
         
         
-#solution2
 
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        XY_set = {}
-        
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                if board[i][j] >'0' and board[i][j]<='9':
-                    if board[i][j] not in XY_set:
-                        XY_set.update({board[i][j]:[[i,j]]})
-                    else:
-                        temp_set = XY_set[board[i][j]]
-                        if i in [item[0] for item in temp_set]:
-                            return False
-                        if j in [item[1] for item in temp_set]:
-                            return False
-                        for item in temp_set:
-                            if int(item[0]/3) == int (i/3) and int(item[1]/3) == int (j/3):
-                                return False
-                    XY_set[board[i][j]] = XY_set[board[i][j]] + [[i,j]]
-                    
-                    
-        return True
+
         
