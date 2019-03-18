ffb2cd47d31942e81f214f1fb2e5893cdfe1d82a
axd8911 <axd8911@hotmail.com>
2019-03-18 15:11:55 -0500

    Create 027_Remove_Element_1st_try.py

---
+++ 027_Remove_Element_1st_try.py
@@ -0,0 +1,18 @@
+'''
+65%
+
+Feel free to overwrite later items to eariler index because eariler index will never be used in the future
+'''
+
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        n = 0
+        for item in nums:
+            if item != val:
+                nums[n] = item
+                n = n + 1
+                
+                
+        return len(nums[:n])
