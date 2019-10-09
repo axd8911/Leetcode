a1a5a26816378f41a6228783ee6f3a72cbedc596
axd8911 <axd8911@hotmail.com>
2019-10-09 10:04:54 -0500

    add comments

--- 2nd_try/9999_playground.py
+++ 2nd_try/9999_playground.py
@@ -1,5 +1,12 @@
-index = [i for i in range(20)]
+# each number, print its 4 hash numbers
 
-id1 = [(45 + 2**i)%(2**8) for i in index]
-
-print (id1)
+e = set()
+for i in range(2016,3000):
+    h1 = (i+i**1)%32
+    h2 = (i+i**2)%32
+    h3 = (i+i**3)%32
+    h4 = (i+i**4)%32
+    print (i, ' ',h1, ' ',h2,' ',h3,' ',h4)
+    if {h1,h2,h3,h4}.issubset(e):
+        break
+    e.update([h1,h2,h3,h4])
