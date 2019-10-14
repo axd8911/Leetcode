0ea7c39fee316b4d5ceb43124b5bf2bfad12aa4b
axd8911 <axd8911@hotmail.com>
2019-10-13 23:27:46 -0500

    minor change

--- 2nd_try/9999_playground.py
+++ 2nd_try/9999_playground.py
@@ -1,6 +1,8 @@
-alldata = set()
-for n in range(101):
-    x = (195 + 2**n)%256
-    alldata.add(x)
-
-print (alldata)
+my = [2010,2013,2007,2004,3200]
+idx = [1,2,3]
+res = []
+for item1 in my:
+    for item2 in idx:
+        print (((item1**2+item1**3)*item2)%32)
+    print (' ')
+print (sorted(res))
