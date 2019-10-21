00cf1b9b06f9c86c3d7e6e8e16272c636e587a8a
axd8911 <axd8911@hotmail.com>
2019-10-20 23:27:01 -0500

    minor change

--- 2nd_try/9999_playground.py
+++ 2nd_try/9999_playground.py
@@ -1,8 +1,5 @@
-my = [1975,1985,1995,2005,2015]
-idx = [1,2]
-res = []
-for item1 in my:
-    for item2 in idx:
-        print ((item1*item2)%64)
-    print (' ')
-print (sorted(res))
+import numpy
+
+a = [[1,1,2],[1,2,1],[2,0,1]]
+b = numpy.linalg.inv(a)
+print (b)
