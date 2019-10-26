61b9b0694f39232ada9333b2639d9fbf5a182c7f
axd8911 <axd8911@hotmail.com>
2019-10-26 00:11:01 -0500

    add questions

---
+++ 2nd_try/1027_Longest_Arithmetic_Sequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestArithSeqLength(self, A: List[int]) -> int:
+
+        dic = collections.defaultdict(int)
+        for i in range(len(A)):
+            for j in range(i+1,len(A)):
+                dic[j,A[j]-A[i]] = dic[i,A[j]-A[i]] + 1
+
+        return max(dic.values())+1
