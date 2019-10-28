0c28b2bac34857ddfc55b1791071676260ff415c
axd8911 <axd8911@hotmail.com>
2019-10-27 23:03:47 -0500

    add notes

--- 2nd_try/1027_Longest_Arithmetic_Sequence.py
+++ 2nd_try/1027_Longest_Arithmetic_Sequence.py
@@ -5,5 +5,5 @@ class Solution:
         for i in range(len(A)):
             for j in range(i+1,len(A)):
                 dic[j,A[j]-A[i]] = dic[i,A[j]-A[i]] + 1
-
+ 
         return max(dic.values())+1
