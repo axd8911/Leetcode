76ab06753730af4ff425aad92c6ab0f536ca7ee1
axd8911 <axd8911@hotmail.com>
2019-11-01 15:44:36 -0500

    add notes

--- mianshi_prep/Quora_prep/phone/0437_Path_Sum_III.py
+++ mianshi_prep/Quora_prep/phone/0437_Path_Sum_III.py
@@ -28,3 +28,42 @@ class Solution:
 
     # dic[currSum] should come later because 算当前位置的情况， 不能考虑截至当前位置所有数字的和（currSum) 减去 截至当前位置所有数字的和（这里不正确的多加了一个和）
     # dic[0] = 1 should be there because 当然有一种和是什么都没有，这样才能考虑从root到某个位置的全局
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+
+        # record the possible summations on each node
+        if not root:
+            return 0
+
+        stack = []
+        total = 0
+        oldSum = {}
+
+        while stack or root:
+            while root:
+                #print ('old： ', root.val,oldSum)
+                newSum = {key+root.val:oldSum[key] for key in oldSum}
+                if root.val in newSum:
+                    newSum[root.val] += 1
+                else:
+                    newSum[root.val] = 1
+
+                if sum in newSum:
+                    total += newSum[sum]
+
+                stack.append((root,newSum))
+                root = root.left
+                oldSum = newSum
+
+            root,oldSum = stack.pop()
+            root = root.right
+
+        return total
