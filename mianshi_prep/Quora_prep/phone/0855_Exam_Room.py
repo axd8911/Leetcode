f17b5fa3c6560c2189addae7eeb96ed996eaaa6c
axd8911 <axd8911@hotmail.com>
2019-11-01 13:27:08 -0500

    some editing

--- mianshi_prep/Quora_prep/phone/0855_Exam_Room.py
+++ mianshi_prep/Quora_prep/phone/0855_Exam_Room.py
@@ -1,26 +1,44 @@
 class ExamRoom:
-    def __init__(self, N: int):
 
+    def __init__(self, N: int):
         self.N = N
-        self.occupy = []
+        self.occu = [(self._dis(-1,N),-1,N)]
 
     def seat(self) -> int:
-        if not self.occupy:
+        gap,x,y = heapq.heappop(self.occu)
+
+        if x == -1:
             res = 0
+        elif y == self.N:
+            res = self.N - 1
         else:
-            d = self.occupy[0]
-            res = 0
-            for i in range(1,len(self.occupy)):
-                if d < (self.occupy[i] - self.occupy[i-1])//2:
-                    d = (self.occupy[i] - self.occupy[i-1])//2
-                    res = (self.occupy[i] + self.occupy[i-1])//2
-            if d<self.N-1-self.occupy[-1]:
-                res = self.N - 1
-        bisect.insort(self.occupy,res)
+            res = (x+y)//2
+        heapq.heappush(self.occu,(self._dis(x,res),x,res))
+        heapq.heappush(self.occu,(self._dis(res,y),res,y))
         return res
 
+
     def leave(self, p: int) -> None:
-        self.occupy.remove(p)
+        for item in self.occu:
+            if item[1] == p:
+                gap1,startp,end = item
+            if item[2] == p:
+                gap2,start,endp = item
+        self.occu.remove((gap1,startp,end))
+        self.occu.remove((gap2,start,endp))
+        heapq.heapify(self.occu)
+        heapq.heappush(self.occu,(self._dis(start,end),start,end))
+
+
+    def _dis(self,start,end):
+        if start == -1:
+            return -end
+        elif end == self.N:
+            return -(self.N-1-start)
+        else:
+            return -((end-start)//2)
+
+
 
 # Your ExamRoom object will be instantiated and called as such:
 # obj = ExamRoom(N)
