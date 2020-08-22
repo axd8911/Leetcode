bd20e75f88a72752a0d2bedc14e977862050ddad
Xudong An <axd8911@hotmail.com>
2020-08-21 21:54:06 -0700

    create JAVA folder

--- Java_solution/0001_Two_Sum.java
+++ Java_solution/0001_Two_Sum.java
@@ -1,6 +1,8 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> exist = new HashMap();
+        HashMap<Integer,Integer> exist = new HashMap<>();
         
         for (int i=0;i<nums.length;i++){
             if (exist.containsKey(target-nums[i])){
