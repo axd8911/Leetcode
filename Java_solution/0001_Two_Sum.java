7602e290494eecabbd0b604dabd63c7d7a78ae10
Xudong An <axd8911@hotmail.com>
2020-08-21 21:43:23 -0700

    create JAVA folder

---
+++ Java_solution/0001_Two_Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> exist = new HashMap();
+        
+        for (int i=0;i<nums.length;i++){
+            if (exist.containsKey(target-nums[i])){
+                return new int[]{exist.get(target-nums[i]),i};
+            }
+            exist.put(nums[i],i);
+        }
+        return new int[]{};
+        
+    }
+}
