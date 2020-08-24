dca30b4a0c054175ffe6a9cd2565462a1f6a22a8
axd8911 <axd8911@hotmail.com>
2020-08-24 14:44:12 -0700

    add question 13

---
+++ Java_solution/0013_Roman_To_Integer.java
@@ -0,0 +1,28 @@
+class RomanToInt {
+    public int romanToInt(String s) {
+        HashMap<Character,Integer> symbol = new HashMap<>();
+        symbol.put('I',1);
+        symbol.put('V',5);
+        symbol.put('X',10);
+        symbol.put('L',50);
+        symbol.put('C',100);
+        symbol.put('D',500);
+        symbol.put('M',1000);
+        
+        int res = 0;
+        
+        for (int i=0;i<s.length()-1;i++){
+            
+            if (symbol.get(s.charAt(i)) >=symbol.get(s.charAt(i+1))){
+                res += symbol.get(s.charAt(i));
+            }
+            else{
+                res -= symbol.get(s.charAt(i));
+            }
+            
+        }
+        
+        res += symbol.get(s.charAt(s.length()-1));
+        return res;
+    }
+}
