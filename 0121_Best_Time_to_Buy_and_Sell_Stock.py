ecf422657d7be3eec4cb3660e62fd60753ab13ef
Xudong An <axd8911@hotmail.com>
2021-05-10 21:35:16 -0700

    Delete 0121_Best_Time_to_Buy_and_Sell_Stock.py

--- 0121_Best_Time_to_Buy_and_Sell_Stock.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        #当前最低点和当前值的差
-        currMin = float('inf')
-        res = 0
-        for item in prices:
-            currMin = min(currMin,item)
-            res = max(res, item-currMin)
-        return res
