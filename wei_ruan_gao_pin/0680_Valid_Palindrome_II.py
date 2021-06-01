b193dc0e9d7622abd561f0d519494d1108e02d70
Xudong An <axd8911@hotmail.com>
2021-06-01 10:04:34 -0700

    Create 0680_Valid_Palindrome_II.py

---
+++ wei_ruan_gao_pin/0680_Valid_Palindrome_II.py
@@ -0,0 +1,11 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if s==s[::-1]:
+            return True
+        l,r=0,len(s)-1
+        while l<=r:
+            if s[l]!=s[r]:
+                return s[l:r]==s[l:r][::-1] or s[l+1:r+1]==s[l+1:r+1][::-1]
+
+            l+=1
+            r-=1
