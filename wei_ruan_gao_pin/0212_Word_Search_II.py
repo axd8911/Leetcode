f8718424e7e2258f1a0be3a128c203d9bb1ab68c
Xudong An <axd8911@hotmail.com>
2021-06-18 17:14:47 -0700

    Create 0212_Word_Search_II.py

---
+++ wei_ruan_gao_pin/0212_Word_Search_II.py
@@ -0,0 +1,51 @@
+class Trie:
+    def __init__(self):
+        self.dict = {}
+        
+    def add(self,word):
+        curr = self.dict
+        for letter in word:
+            if letter not in curr:
+                curr[letter] = {}
+            curr = curr[letter]
+        curr['*'] = word
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        trie = Trie()
+        for item in words:
+            trie.add(item)
+        res = set()
+        h = len(board)
+        w = len(board[0])
+        #print (trie.dict)
+        visited = set()
+        
+        
+        def dfs(currDict,i,j):
+            
+            char = board[i][j]
+            currDict = currDict[char]
+            
+            if '*' in currDict:
+                res.add(currDict["*"])
+            
+            visited.add((i,j))
+            for dx,dy in [(i+1,j),(i,j+1),(i-1,j),(i,j-1)]:
+                
+                if (dx,dy) not in visited and 0<=dx<h and 0<=dy<w and board[dx][dy] in currDict:
+                    
+                    dfs(currDict,dx,dy)
+            visited.remove((i,j))
+        
+        for i in range(h):
+            for j in range(w):
+                if board[i][j] in trie.dict:
+                    dfs(trie.dict,i,j)
+                
+        
+        return res
+        
+
+        
